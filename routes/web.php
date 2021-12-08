9eb1984b5c18c54ce8137ab8db217fc5d3ecbd65
rfqalfauzaan <alfauzaan.rifqi@gmail.com>
2021-12-08 15:56:50 +0700

    Membuat routes
    disini kami mengerjakan bareng bareng di discord jadi  ngepush dari saya dulu setelah kita punya tugas masing masing anggota mengerjakan tampilan
    
    Co-Authored-By: aimar25 <60495741+aimar25@users.noreply.github.com>
    Co-Authored-By: fikrianjani <60495650+fikrianjani@users.noreply.github.com>
    Co-Authored-By: ipansopian <60495718+ipansopian@users.noreply.github.com>
    Co-Authored-By: lukmantresnahadi <60495843+lukmantresnahadi@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/canteen', function () {
+    return view('canteen');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
