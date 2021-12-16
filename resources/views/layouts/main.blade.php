2668ef101d66308dd3ed68719b12c06eb3afde47
lukmantresnahadi <60495843+lukmantresnahadi@users.noreply.github.com>
2021-12-16 22:06:23 +0700

    memperbaiki navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,7 +18,9 @@
         @yield('container')
       </div>
    
-
+    {{-- bootstrap icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
+    
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
